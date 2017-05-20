1e4777733d75fa65c7fc9c1fa7765804b55e8479
sbvorpagel <sbvorpagel@gmail.com>
2017-05-20 11:40:46 -0300

    Adicionado user mongodb.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var mongoose = require('mongoose');
 // Use native Node promises
 mongoose.Promise = global.Promise;
 // connect to MongoDB
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds149221.mlab.com:49221/heroku_n7zlcwzm')
+mongoose.connect('mongodb://heroku_n7zlcwzm:onog6vg47gc022qch9lit63f5u@ds149221.mlab.com:49221/heroku_n7zlcwzm')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
